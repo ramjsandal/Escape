307a9cacb5d3e20637d007db747fddde3302045f
ramjsandal <ramjsandal@gmail.com>
2023-02-19 11:32:52 -0500

    added cooldown to gun and painted the scene

--- wentworthjam/Assets/Scripts/ShootBullet.cs
+++ wentworthjam/Assets/Scripts/ShootBullet.cs
@@ -8,19 +8,24 @@ public class ShootBullet : MonoBehaviour
 
     [SerializeField] private GameObject bullet;
     [SerializeField] private AudioClip bulletSFX;
+    [SerializeField] private float cooldown;
+    private float _currentCooldown;
     void Start()
     {
-        
+        _currentCooldown = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && _currentCooldown <= 0)
         {
             Transform t = transform;
             GameObject instance = Instantiate(bullet, 
                 t.position + t.forward, t.rotation);
+            _currentCooldown = cooldown;
         }
+
+        _currentCooldown -= Time.deltaTime;
     }
 }
