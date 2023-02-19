a8ce5413e52821eaafaac47351daf0d166f999a5
ramjsandal <ramjsandal@gmail.com>
2023-02-19 14:21:18 -0500

    made transition to you win screen

--- wentworthjam/Assets/Scripts/UILevelManager.cs
+++ wentworthjam/Assets/Scripts/UILevelManager.cs
@@ -17,7 +17,8 @@ public class UILevelManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 
     // Update is called once per frame
