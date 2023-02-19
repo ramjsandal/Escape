675890ce4e08940c779c8abc1c15042fe8e8ab8f
ramjsandal <ramjsandal@gmail.com>
2023-02-19 14:04:45 -0500

    finished main menu, credits scene, and help scene

--- wentworthjam/Assets/Scripts/UILevelManager.cs
+++ wentworthjam/Assets/Scripts/UILevelManager.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.MemoryProfiler;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UILevelManager : MonoBehaviour
 {
     [SerializeField] private string nextScene;
+
+    [SerializeField] private string menuScene;
+    
+    [SerializeField] private string creditsScene;
+    
+    [SerializeField] private string helpScene;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -21,5 +29,20 @@ public class UILevelManager : MonoBehaviour
     public void LoadNextScene()
     {
         SceneManager.LoadScene(nextScene);
+    }
+
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene(creditsScene);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(menuScene);
+    }
+
+    public void LoadHelp()
+    {
+        SceneManager.LoadScene(helpScene);
     }
 }
